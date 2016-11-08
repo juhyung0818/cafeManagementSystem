333e2d4f15aa1f8461f66d49775ceeeac411be6f
윤주형 <juhyung0818@naver.com>
2016-11-08 18:29:54 +0900

    add cafe: register, display list

---
+++ CafeManageMentSystem/src/main/java/com/cafe/controller/CafeController.java
@@ -0,0 +1,16 @@
+package com.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Cafeteria Controller class
+ * @author YJH
+ * 2016.11.08.Thu
+ */
+
+@Controller
+@RequestMapping("/cafe")
+public class CafeController {
+
+}
